10dd352a0af3407a04241913a4854559e1198aa0
reneafranco <reneadonayfranco@gmail.com>
2024-08-12 21:06:50 -0400

    over - hh

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "ReneBlog",
+  url: "https://example.com",
+  description: "Teaching Blog using Next velite and MDX",
+  author: "Rene Franco",
+  links: {
+    twitter: "https://github.com/reneafranco",
+    github: "https://github.com/reneafranco",
+    personalSite: "https://www.reneafranco.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
