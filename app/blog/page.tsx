d3db34549cfd488be1965c75798d37b1338743c9
reneafranco <reneadonayfranco@gmail.com>
2024-08-15 17:08:35 -0400

    blog page --s

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default async function BlogPage() {
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between gap-8 ">
+        <div className="flex-1 space-y-4"></div>
+      </div>
+    </div>
+  );
+}
