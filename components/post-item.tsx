1054cfa73b5c0433086c79e5511ad4731c1ebdb1
reneafranco <reneadonayfranco@gmail.com>
2024-08-15 17:31:12 -0400

    blog-page-items-description

---
+++ components/post-item.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface PostItemProps {
+  slug: string;
+  title: string;
+  description?: string;
+  date: string;
+}
+
+export function PostItem({ slug, title, description, date }: PostItemProps) {
+  return (
+    <article className="flex flex-col gap-2 border-border border-b py-31">
+      <div>
+        <h2 className="text-2xl font-bold">
+          <Link href={slug}>{title}</Link>
+        </h2>
+      </div>
+      <div className="max-w-none text-muted-foreground">{description}</div>
+    </article>
+  );
+}
